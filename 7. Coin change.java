8b270cff633a707f084296e4efe7c7c09da27626
Shambhavi Awasthi <shambhavi020898@gmail.com>
2020-06-12 14:09:12 +0530

    Add files via upload

---
+++ 7. Coin change.java
@@ -0,0 +1,16 @@
+// Shambhavi Awasthi
+// LEETCODE MAY CHALLENGE
+// DAY 7: Coin Change 2
+
+class Solution {
+    public int change(int amount, int[] coins) {
+       int arr[]=new int[amount+1];
+        arr[0]=1;
+        for(int i=0;i<coins.length;i++){
+            for(int j=coins[i];j<=amount;j++){
+                arr[j]+=arr[j-coins[i]];
+            }
+        }
+        return arr[amount];
+    }
+}
