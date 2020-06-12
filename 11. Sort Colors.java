35485f47a3b6a5cce37ff0de0bffd74d238220b7
Shambhavi Awasthi <shambhavi020898@gmail.com>
2020-06-12 12:28:46 +0530

    Add files via upload

---
+++ 11. Sort Colors.java
@@ -0,0 +1,31 @@
+// Shambhavi Awasthi
+// LEETCODE MAY CHALLENGE
+// DAY 11: Sort Colors
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int count[]=new int[3];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0)
+                count[0]++;
+            else if(nums[i]==1)
+                count[1]++;
+            else
+                count[2]++;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(count[0]!=0){
+                nums[i]=0;
+                    count[0]--;
+            }
+        else if(count[1]!=0){
+                nums[i]=1;
+                    count[1]--;
+            }
+        else{
+                nums[i]=2;
+                    count[2]--;
+            }
+        }
+    }
+}
