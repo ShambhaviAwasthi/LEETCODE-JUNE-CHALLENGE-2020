14122570b1e81c7c90a18841aa2e967203953f85
Shambhavi Awasthi <shambhavi020898@gmail.com>
2020-06-13 10:47:13 +0530

    Add files via upload

---
+++ 9.  Is Subsequence.java
@@ -0,0 +1,20 @@
+// Shambhavi Awasthi
+// LEETCODE MAY CHALLENGE
+// DAY 9: is Subsequence
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        if(s.length()==0)
+            return true;
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j))
+                i++;
+            j++;
+            if(i==s.length())
+                return true;
+        }
+        return false;
+    }
+}
