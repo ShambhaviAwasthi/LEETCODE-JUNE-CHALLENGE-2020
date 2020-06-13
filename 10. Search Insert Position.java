a0b48270f98afe51d56ea4dc64f3684eea9d7bdd
Shambhavi Awasthi <shambhavi020898@gmail.com>
2020-06-13 10:58:56 +0530

    Add files via upload

---
+++ 10. Search Insert Position.java
@@ -0,0 +1,20 @@
+// Shambhavi Awasthi
+// LEETCODE MAY CHALLENGE
+// DAY 10: Search Insert Position
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(target>nums[nums.length-1]){
+        return nums.length;
+    }
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+            int m=start+(end-start)/2;
+            if(target>nums[m])
+                start=start+1;
+            else
+                end=m;
+        }
+        return start;
+    }
+}
